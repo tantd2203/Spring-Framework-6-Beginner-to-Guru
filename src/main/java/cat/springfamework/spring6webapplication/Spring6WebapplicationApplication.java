65812f20351612f31b8e11705463850d5dd80ddb
tantd9 <tranduytan.220300@gmail.com>
2023-02-23 09:35:38 +0700

    start-project

---
+++ src/main/java/cat/springfamework/spring6webapplication/Spring6WebapplicationApplication.java
@@ -0,0 +1,13 @@
+package cat.springfamework.spring6webapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebapplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6WebapplicationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cat/springfamework/spring6webapplication/Spring6WebapplicationApplicationTests.java
@@ -0,0 +1,13 @@
+package cat.springfamework.spring6webapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebapplicationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
