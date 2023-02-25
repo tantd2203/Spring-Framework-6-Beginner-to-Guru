3613ff3f083992aa29f25831159ec288037afa8d
tantd9 <tranduytan.220300@gmail.com>
2023-02-25 16:13:12 +0700

    frist conmmit  section 2

---
+++ src/main/java/cat/Spring6DiApplication.java
@@ -0,0 +1,13 @@
+package cat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring6DiApplication.class, args);
+	}
+
+}
